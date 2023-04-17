0e5f4c0ac7e2c84a9021473137261173bf912d81
Vasiliy Gorilev <89159793029@ya.ru>
2023-04-17 13:02:08 +0300

    Решаю задачу 2

--- DZ_01.java
+++ DZ_01.java
@@ -1,13 +1,13 @@
 import java.util.Scanner;
 import java.text.DecimalFormat;
-import java.util.ArrayList;
+import java.util.Arrays;
 
 public class DZ_01 {
     public static void main(String[] args) {
 
         Scanner iScanner = new Scanner(System.in);
         System.out.println(
-                "Задачи:\n1.1 Вычислить n-ое треугольного число.\n1.2 Вычислить n факториал.\n2 Вывести все простые числа от 1 до 1000.\n2. Вывести все простые числа от 1 до 1000.\n3. Простой калькулятор (выход осуществляется командой stop)");
+                "Задачи:\n1.1 Вычислить n-ое треугольного число.\n1.2 Вычислить n факториал.\n2. Вывести все простые числа от 1 до 1000.\n3. Простой калькулятор (выход осуществляется командой stop)");
         System.out.printf("Введите номер задачи для проверки: ");
         String numb = iScanner.nextLine();
         switch (numb) {
@@ -23,7 +23,7 @@ public class DZ_01 {
                 break;
             case "2":
                 System.out.println("Все простые числа от 1 до 1000");
-
+                System.out.println(primeNumber());
                 break;
             case "3":
                 System.out.println("Выход осуществляется командой stop");
@@ -57,6 +57,7 @@ public class DZ_01 {
                 break;
 
             default:
+                System.out.println("Введён не верный номер.");
                 break;
         }
         iScanner.close();
@@ -83,13 +84,29 @@ public class DZ_01 {
         return result;
     }
 
-    // public static ArrayList<Integer> primeNumber() {
-    // ArrayList<Integer> list = new ArrayList<>();
-    // for (int i = 1; i < 1000; i++) {
+    /**
+     * @return
+     */
+    public static int[] primeNumber() {
+    int[] myArray = new int[20];
 
-    // }
+    for (int i = 1; i <= 20; i++) {
+        myArray[i - 1] = i;
+    }
 
-    // }
+    
+    for (int i = 2; i < myArray.length; i++) {
+        if (myArray[i] % i == 0 && myArray[i] / i != 1) {
+
+        }
+    }
+
+    for (int i = 0; i < myArray.length; i++) {
+        System.out.print(myArray[i] + " ");
+        
+    }
+    return myArray;
+    }
 
     public static String ResultCalc(Double a, Double b, char operation) {
         Double result = 0.0;
