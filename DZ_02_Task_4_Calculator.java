3e4b334fb9b6a81e46bf3698e9ff48a1d71bc371
Vasiliy Gorulev <89159793029@ya.ru>
2023-04-22 15:33:49 +0300

    Добавил отсечки времени в логирование

--- DZ_02_Task_4_Calculator.java
+++ DZ_02_Task_4_Calculator.java
@@ -1,39 +1,50 @@
 import java.io.FileWriter;
 import java.io.IOException;
 import java.text.DecimalFormat;
+import java.time.LocalDateTime;
+import java.util.Locale;
 import java.util.Scanner;
+
+import javax.xml.crypto.Data;
 
 /**
  * DZ_02.Task_4.Calculator
  */
 public class DZ_02_Task_4_Calculator {
     static Scanner iScanner = new Scanner(System.in);
-
+    static LocalDateTime startCalc = LocalDateTime.now();
     public static void main(String[] args) throws IOException {
         DecimalFormat dF = new DecimalFormat("#.###");
         // Scanner iScanner = new Scanner(System.in);
-        System.out.println("Выход осуществляется командой stop на этапе ввода действия");
-        Boolean flag = true;
-        while (flag == true) {
-            System.out.print("Введите число 1: ");
-            Double a = iScanner.nextDouble();
-            System.out.print("Введите действие (+, -, *, /, %): ");
-            char operation = iScanner.next().charAt(0);
-            System.out.print("Введите число 2: ");
-            Double b = iScanner.nextDouble();
-            String resultat = ResultCalc(a, b, operation);
-            if (operation == 's' || operation == 'S')
-                flag = false;
-            else {
-                System.out.printf("%s %s %s = %s\n", dF.format(a), operation, dF.format(b), resultat);
-                try (FileWriter fw = new FileWriter("logCalc.log", true)) {
+        try (FileWriter fw = new FileWriter("logCalc.log", true)) {
+            System.out.println("Выход осуществляется командой stop на этапе ввода действия");
+            fw.write("Запуск " + String.valueOf(startCalc) + '\n');
+            
+            Boolean flag = true;
+            while (flag == true) {
+                System.out.print("Введите число 1: ");
+                Double a = iScanner.nextDouble();
+                // Double a = 4.0;
+                System.out.print("Введите действие (+, -, *, /, %): ");
+                char operation = iScanner.next().charAt(0);
+                // char operation = '+';
+                System.out.print("Введите число 2: ");
+                Double b = iScanner.nextDouble();
+                // Double b = 4.0;
+                String resultat = ResultCalc(a, b, operation);
+                if (operation == 's' || operation == 'S'){
+                    fw.write("end program\n");
+                    flag = false;
+                }
+                else {
+                    System.out.printf("%s %s %s = %s\n", dF.format(a), operation, dF.format(b), resultat);
                     fw.write(String.valueOf(a) + " ");
                     fw.write(String.valueOf(operation) + " ");
                     fw.write(String.valueOf(b) + " = ");
                     fw.write(String.valueOf(resultat) + '\n');
-                    fw.flush();
-                }
+                };
             }
+            fw.flush();
         }
         iScanner.close();
     }
@@ -41,38 +52,27 @@ public class DZ_02_Task_4_Calculator {
     public static String ResultCalc(Double a, Double b, char operation) {
         Double result = 0.0;
 
-        if (operation == '+')
-            result = a + b;
-        else if (operation == '-')
-            result = a - b;
-        else if (operation == '*')
-            result = a * b;
-        else if (operation == '/')
-            result = a / b;
-        else if (operation == '%')
-            result = a % b;
+        switch (operation) {
+            case '+':
+                result = a + b;
+                break;
+            case '-':
+                result = a - b;
+                break;
+            case '*':
+                result = a * b;
+                break;
+            case '/':
+                result = a / b;
+                break;
+            case '%':
+                result = a % b;
+                break;
 
-        // switch (operation) {
-        // Float result = 0;
-        // case "+":
-        // result = a + b;
-        // break;
-        // case "-":
-        // result = a - b;
-        // break;
-        // case "*":
-        // result = a * b;
-        // break;
-        // case "/":
-        // result = a / b;
-        // break;
-        // case "%":
-        // result = a % b;
-        // break;
+            default:
+                break;
+        }
 
-        // default:
-        // break;
-        // }
         DecimalFormat dF = new DecimalFormat("#.###");
         String Res = dF.format(result);
         return Res;
