56181c2da8cc31f8dcd307828ecaba123d31c911
Vasiliy Gorulev <89159793029@ya.ru>
2023-04-22 22:19:18 +0300

    решаю задачу

---
+++ DZ_03.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class DZ_03 {
+    static Scanner iScanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        // 1. Алгоритм сортировки слиянием
+        int[] arrayNumbs = { 77, 46, 11, 89, 48, 14, 67, 73, 22, 26, 47, 1, 67, 23, 4 };
+        
+
+
+
+
+        
+
+        iScanner.close();
+    }
+}
