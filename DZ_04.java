0bb82a583ca4ee801b8d84a36487f43bb7a2a405
Vasiliy Gorulev <89159793029@ya.ru>
2023-05-09 12:48:30 +0300

    добавил решение задачи 2

--- DZ_04.java
+++ DZ_04.java
@@ -5,21 +5,30 @@ import java.util.Queue;
 
 public class DZ_04 {
     public static void main(String[] args) {
-        
+
         Integer[] userInput = new Integer[] { 77, 46, 11, 89, 48, 14, 67, 73, 22, 26, 47, 1, 67, 23, 4 };
-        
-        // Задача 1. я подумал, что будет правильней принять список и в функции вернуть тоже список.
-        /* System.out.println(Arrays.toString(userInput));
-        System.out.println(Arrays.toString(ConvertList(userInput))); */
+
+        // Задача 1. я подумал, что будет правильней принять список и в функции вернуть
+        // тоже список.
+        /*
+         * System.out.println(Arrays.toString(userInput));
+         * System.out.println(Arrays.toString(ConvertList(userInput)));
+         */
 
         // Задача 2.
-        LinkedList<Integer> testList = new LinkedList<>(Arrays.asList(userInput));
-        System.out.println(List.copyOf(testList));
-        Queue <Integer> stepQueue = new Queue<>();
-        
+        // LinkedList<Integer> testList = new LinkedList<>(Arrays.asList(userInput));
+        // System.out.println(List.copyOf(testList));
+        Queue<Integer> stepQueue = new LinkedList();
+        for (int i = 0; i < userInput.length; i++) {     // применение метода enqueue
+            stepQueue.add(userInput[i]);
+        }
+        System.out.println(String.valueOf(stepQueue)); 
+        System.out.println("dequeue: " + stepQueue.poll() + " <- " + String.valueOf(stepQueue));   // применение метода dequeue
+        System.out.println("first: " + stepQueue.peek() + " <- " + String.valueOf(stepQueue));   // применение метода first
+
+
         // Задача 3
         // вынесена в отдельный файл Task_Calculator.java
-
 
     }
 
@@ -27,7 +36,7 @@ public class DZ_04 {
         LinkedList<Integer> listOne = new LinkedList<>(Arrays.asList(userInput));
         LinkedList<Integer> resulList = new LinkedList<>();
         int size = listOne.size();
-        for (int i = 0; i < size; i++) 
+        for (int i = 0; i < size; i++)
             resulList.addFirst(listOne.removeFirst());
         Integer[] result = resulList.toArray(new Integer[resulList.size()]);
         return result;
