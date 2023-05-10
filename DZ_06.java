19f6e77bba359ee9739396b1f66bf55da0a078c9
Vasiliy Gorulev <89159793029@ya.ru>
2023-05-10 20:37:41 +0300

    Делаю домашнее задание

---
+++ DZ_06.java
@@ -0,0 +1,13 @@
+public class DZ_06 {
+    public static void main(String[] args) {
+        
+    }
+
+    class Laptops {
+        Integer operationMamory;
+        Integer diskMemory;
+        String nameSystem;
+        String colors;
+        
+    }
+}
